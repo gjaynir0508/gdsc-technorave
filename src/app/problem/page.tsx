e3cda8d5415bf5568fd50cdf6054097d37cae363
Jayanth Gudimella <gjaynir0508@gmail.com>
2024-02-23 22:29:52 +0530

    Add suspense boundary to problem/page.tsx

--- src/app/problem/page.tsx
+++ src/app/problem/page.tsx
@@ -12,25 +12,27 @@ export default function Problem() {
 		redirect("/");
 	}
 	return (
-		<main className="bg-cyan-200 bg-opacity-25 h-screen grid place-items-center">
-			<div className="flex flex-col gap-4 justify-end">
-				<h2 className="text-md mb-8 italic">
-					⚠️ Uh Oh! An Error Occurred.
-				</h2>
-				<h1 className="text-3xl font-bold">✉️ {msg}</h1>
-				<h3 className="text-2xl font-semibold">🦉 {sol}</h3>
-				<div className="flex gap-4 mt-8">
-					<button
-						onClick={
-							// Attempt to recover by trying to re-render the segment
-							() => router.push("/")
-						}
-						className="bg-slate-500 text-white px-8 py-2 rounded-md"
-					>
-						Try again
-					</button>
+		<React.Suspense>
+			<main className="bg-cyan-200 bg-opacity-25 h-screen grid place-items-center">
+				<div className="flex flex-col gap-4 justify-end">
+					<h2 className="text-md mb-8 italic">
+						⚠️ Uh Oh! An Error Occurred.
+					</h2>
+					<h1 className="text-3xl font-bold">✉️ {msg}</h1>
+					<h3 className="text-2xl font-semibold">🦉 {sol}</h3>
+					<div className="flex gap-4 mt-8">
+						<button
+							onClick={
+								// Attempt to recover by trying to re-render the segment
+								() => router.push("/")
+							}
+							className="bg-slate-500 text-white px-8 py-2 rounded-md"
+						>
+							Try again
+						</button>
+					</div>
 				</div>
-			</div>
-		</main>
+			</main>
+		</React.Suspense>
 	);
 }
